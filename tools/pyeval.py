98714c72158b480e79ae8843150a4758f10a2578
bergi <s.berke@netzkolchose.de>
2018-06-16 01:46:58 +0200

    time-out for evaluate_python

--- tools/pyeval.py
+++ tools/pyeval.py
@@ -1,11 +1,18 @@
 import subprocess
 import sys
+import multiprocessing
+import queue
+
+__all__ = ("evaluate_python",)
+
+MAX_SECONDS = 4
 
 
-def evaluate_python(code):
+def evaluate_python_queue(code, queue):
     python = sys.executable
     code = """
 import random
+from math import *
 print(%s)
 """ % code
     try:
@@ -13,8 +20,24 @@ print(%s)
             [python, "-c", code], stderr=subprocess.PIPE,
         )
     except subprocess.CalledProcessError:
-        return "What?"
-    return ret.decode("utf-8").strip()
+        queue.put("What?")
+        return
+    queue.put(ret.decode("utf-8").strip())
+
+
+def evaluate_python(code):
+    q = multiprocessing.Queue()
+    thread = multiprocessing.Process(target=lambda: evaluate_python_queue(code, q))
+    thread.start()
+
+    try:
+        ret = q.get(timeout=MAX_SECONDS)
+    except queue.Empty:
+        thread.terminate()
+        return "Sorry, das dauerte mir zu lang.."
+
+    thread.join()
+    return ret
 
 
 if __name__ == "__main__":
@@ -22,4 +45,5 @@ if __name__ == "__main__":
     print(evaluate_python('"*" * 10'))
     print(evaluate_python("'ä' * 10"))
     print(evaluate_python("blub"))
+    #print(evaluate_python("time.sleep(10)"))
 
